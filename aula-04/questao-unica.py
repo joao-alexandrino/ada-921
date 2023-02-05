a485b612036a54c77172cbf40761657397d6c6cf
joao-alexandrino <joaovictormotaa@arquitetura.ufc.br>
2023-02-05 19:42:03 -0300

    add aula-04/questao-unica

---
+++ aula-04/questao-unica.py
@@ -0,0 +1,22 @@
+# Solução
+
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\joaov\Downloads\alunos3.csv', sep = ';', decimal = ',', usecols= [0,3,4,5,6])
+
+df['Media'] = df.apply(lambda row:
+    row[1:5].mean(),
+    axis = 1
+)
+
+
+# Solução alternativa
+
+# df['Media'] = df.iloc[:,[1, 2, 3, 4]].mean(axis = 1)
+
+
+# continuação do ecercício
+
+print(df)
+
+df[["RA","Media"]].to_excel(r'C:\Users\joaov\Downloads\medias.xlsx', sheet_name= 'Medias')
