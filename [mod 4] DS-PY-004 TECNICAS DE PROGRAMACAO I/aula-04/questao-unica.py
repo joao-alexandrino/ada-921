3b62c8a816836bc49e1442cb5f6b6698082fe0e9
joao-alexandrino <joaovictormotaa@arquitetura.ufc.br>
2023-02-06 19:29:26 -0300

    add aula-04/questao-unica

--- [mod 4] DS-PY-004 TECNICAS DE PROGRAMACAO I/aula-04/questao-unica.py
+++ [mod 4] DS-PY-004 TECNICAS DE PROGRAMACAO I/aula-04/questao-unica.py
@@ -15,8 +15,15 @@ df['Media'] = df.apply(lambda row:
 # df['Media'] = df.iloc[:,[1, 2, 3, 4]].mean(axis = 1)
 
 
+# Solução alternativa
+
+# df['Media'] = df[['Prova_1', 'Prova_2', 'Prova_3', 'Prova_4']].mean(axis = 1)
+
+
 # continuação do ecercício
 
 print(df)
 
-df[["RA","Media"]].to_excel(r'C:\Users\joaov\Downloads\medias.xlsx', sheet_name= 'Medias')
+# df[["RA","Media"]].to_excel(r'C:\Users\joaov\Downloads\medias.xlsx', sheet_name= 'Medias')
+
+df[["RA","Media"]].to_excel(r'C:\Users\joaov\Downloads\medias_2.xlsx', index = False)
